cde2fa34bcd8a2197c6a1ee95c85f3652f893f71
Griffin Riley <rileyg@oregonstate.edu>
2024-03-18 14:43:33 -0700

    finished part 2

--- solution.java
+++ solution.java
@@ -1,9 +1,13 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Collections;
 
 public class solution {
         public static void main(String []args) throws Exception {
-            solvepart1();
+            solvepart2();
         }
 
         private static void solvepart1() throws Exception {
@@ -12,6 +16,7 @@ public class solution {
             String[] splitdata = inputdata.split(System.lineSeparator());
             
             for (int i = 0; i < splitdata.length; i++) {
+                /*do for each line of file*/
                 String line = splitdata[i];
                 boolean isFirst = true;
                 char firstDigit = '0';
@@ -24,6 +29,64 @@ public class solution {
                             isFirst = false;
                         }
                         secondDigit = character;
+                    }
+                }
+                int total = (Character.getNumericValue(firstDigit) * 10) + Character.getNumericValue(secondDigit);
+                sum = sum + total;
+            }
+            
+            System.out.println(sum);
+        }
+
+        private static void solvepart2() throws Exception {
+            Dictionary<String, Character> nums = new Hashtable<>();
+            nums.put("one", '1');
+            nums.put("two", '2');
+            nums.put("three", '3');
+            nums.put("four", '4');
+            nums.put("five", '5');
+            nums.put("six", '6');
+            nums.put("seven", '7');
+            nums.put("eight", '8');
+            nums.put("nine", '9');
+            
+            int sum = 0;
+            String inputdata = fileRead("input.txt");
+            String[] splitdata = inputdata.split(System.lineSeparator());
+            
+            for (int i = 0; i < splitdata.length; i++) {
+                /* do for each line of file*/
+                String line = splitdata[i];
+                boolean isFirst = true;
+                char firstDigit = '0';
+                char secondDigit = '0';
+                for (int j = 0; j < line.length(); j++) {
+                    /* do for each char of line */
+                    char character = line.charAt(j);
+                    if (Character.isDigit(character)) {
+                        if (isFirst) {
+                            firstDigit = character;
+                            isFirst = false;
+                        }
+                        secondDigit = character;
+                    } else {
+                        int k = j;
+                        while(k <= line.length() && k-j < 6) {
+                            String subline = line.substring(j,k);
+                            Enumeration numsAsStrs = nums.keys();
+                            while(numsAsStrs.hasMoreElements()) {
+                                String numStr = numsAsStrs.nextElement().toString();
+                                if (subline.equals(numStr)) {
+                                    if (isFirst) {
+                                        firstDigit = nums.get(numStr);
+                                        isFirst = false;
+                                    }
+                                    secondDigit = nums.get(numStr);
+                                    k = j + 6;
+                                }
+                            }
+                            k++;
+                        }
                     }
                 }
                 int total = (Character.getNumericValue(firstDigit) * 10) + Character.getNumericValue(secondDigit);
